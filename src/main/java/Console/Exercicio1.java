5ef7a5309263642c4902aae0e39a41cfb0d045ce
Lara Pereira <larabernspereira@gmail.com>
2025-06-04 19:54:52 -0300

    add exer 1

---
+++ src/main/java/Console/Exercicio1.java
@@ -0,0 +1,39 @@
+package Console;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Exercicio1 {
+    public static void main(String[] args) throws Exception {
+        try {
+            String path = "https://apichallenges.eviltester.com/sim/entities";
+            URL url = new URL(path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            conn.setConnectTimeout(10_000);
+
+            int status = conn.getResponseCode();
+            System.out.println("Código de status HTTP: " + status);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            StringBuilder response = new StringBuilder();
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println("Resposta do servidor:");
+            System.out.println(response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
